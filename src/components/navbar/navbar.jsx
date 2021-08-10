ee07c2047ad31ac36f9fcca0df63ebdf709192ec
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-08-10 17:48:36 +0900

    finish Navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,11 +4,14 @@ import styles from './navbar.module.css';
 
 const Navbar = ({ setQuery }) => (
   <div className={styles.container}>
-    <img
-      className={styles.logo}
-      src="https://cdn.icon-icons.com/icons2/2699/PNG/512/youtube_logo_icon_168737.png"
-      alt="youtube logo"
-    />
+    <div className={styles.logo_container}>
+      <img
+        className={styles.logo}
+        src="https://cdn.icon-icons.com/icons2/2699/PNG/512/youtube_logo_icon_168737.png"
+        alt="youtube logo"
+      />
+      <span className={styles.main_title}>JvnTube</span>
+    </div>
     <Searchbar setQuery={setQuery} />
     <div className={styles.profile_container}>
       <img
