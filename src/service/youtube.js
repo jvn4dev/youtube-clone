f1532387a06eed19d3615b6edc92293d4bba7447
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-08-13 14:15:07 +0900

    change promise chain to async, await func

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -6,27 +6,32 @@ class Youtube {
   }
 
   // 가장 유명한 영상 목록을 불러와 리턴함
-  mostPopular() {
+  async mostPopular() {
     const url = `https://www.googleapis.com/youtube/v3/videos?key=${this.key}&part=snippet&maxResults=25&chart=mostPopular`;
-    return axios
-      .get(url)
-      .then((rsp) => rsp.data)
-      .then((data) => data.items)
-      .catch((err) => console.log('error: ' + err));
+    try {
+      const rsp = await axios.get(url);
+      const data = rsp.data;
+      return data.items;
+    } catch (err) {
+      return console.log('error: ' + err);
+    }
   }
 
   // 쿼리로 날린 영상들을 불러옴
-  search(qry) {
+  async search(qry) {
     const url = `https://www.googleapis.com/youtube/v3/search?key=${this.key}&part=snippet&maxResults=25&q=${qry}&type=video&key=AIzaSyDvzt0gZhixUuOHBNkl46RwE8lnSLFmSt0`;
 
-    return axios
-      .get(url)
-      .then((rsp) => rsp.data)
-      .then((result) => {
-        return result.items.map((item) => ({ ...item, id: item.id.videoId }));
-      })
-      .then((items) => items)
-      .catch((err) => console.log('error: ' + err));
+    try {
+      const rsp = await axios.get(url);
+      const result = rsp.data;
+      const items = result.items.map((item) => ({
+        ...item,
+        id: item.id.videoId,
+      }));
+      return items;
+    } catch (err) {
+      return console.log('error: ' + err);
+    }
   }
 }
 
